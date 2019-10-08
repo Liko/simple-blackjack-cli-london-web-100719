47d8763e5f2561292bf4d80781ee24020b84363d
Li Xie <li_xie1992@hotmail.com>
2019-10-08 08:22:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,7 +54,7 @@ def runner
   welcome 
   card_total = initial_round
   display_card_total
-  until card_total > 21 [do]
+  until card_total > 21 do
     card_total = hit?(card_total)
   end
   end_game(card_total)
